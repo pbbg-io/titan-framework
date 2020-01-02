7f091c46241066f47bb343f4902737522f885092
Ian <hi@ian.pe>
2020-01-02 21:57:26 +0000

    Made the layout slightly more responsive

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -28,7 +28,7 @@
 <div id="wrapper">
 
     <!-- Sidebar -->
-    <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+    <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion d-none d-md-block" id="accordionSidebar">
 
         <!-- Sidebar - Brand -->
         <a class="sidebar-brand d-flex align-items-center justify-content-center" href="/">
@@ -188,17 +188,20 @@
                             <i class="fas fa-search fa-fw"></i>
                         </a>
                         <!-- Dropdown - Messages -->
-                        <div class="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in" aria-labelledby="searchDropdown">
-                            <form class="form-inline mr-auto w-100 navbar-search">
-                                <div class="input-group">
-                                    <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
-                                    <div class="input-group-append">
-                                        <button class="btn btn-primary" type="button">
-                                            <i class="fas fa-search fa-sm"></i>
-                                        </button>
-                                    </div>
+                        <div class="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in"
+                             aria-labelledby="searchDropdown">
+
+                            {!! \Form::open()->route('admin.search')->attrs(['class'    =>  'form-inline mr-auto w-100 navbar-search']) !!}
+                            <div class="input-group">
+                                <input type="text" class="form-control bg-light border-0 small" name="search"
+                                       placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
+                                <div class="input-group-append">
+                                    <button class="btn btn-primary" type="button" onclick="submit()">
+                                        <i class="fas fa-search fa-sm"></i>
+                                    </button>
                                 </div>
-                            </form>
+                            </div>
+                            {!! \Form::close() !!}
                         </div>
                     </li>
                     <!-- Nav Item - User Information -->

c4e25dcf6f8a235af31d310b54754004ce9e020a
Kyle Ellis <ky.ellis83@gmail.com>
2020-01-16 04:13:46 -0800

    Removed settings from Admin Home page

--- src/Http/Controllers/Admin/HomeController.php
+++ src/Http/Controllers/Admin/HomeController.php
@@ -29,20 +29,10 @@ class HomeController extends Controller
     {
         $stats = [];
         $stats['players'] = User::count();
-        $stats['version_update'] = $this->needsEngineUpdate();
         $stats['current_version'] = config('titan.version');
-        $stats['latest_version'] = resolve('settings')->firstWhere('key', 'remote_version')->value ?? 0;
         $stats['players_online'] = User::where('last_move', '>', (new Carbon())->subHour())->count();
         $stats['players_registered_today'] = User::where('created_at', '>', (new Carbon())->startOfDay())->count();
 
         return view('titan::admin.home', compact('stats'));
-    }
-
-    public function needsEngineUpdate(): bool
-    {
-        $remoteEngineVersion = resolve('settings')->firstWhere('key', 'remote_version')->value ?? 0;
-        $remoteVersion = version_compare(config('titan.version'), $remoteEngineVersion);
-
-        return $remoteVersion === -1;
     }
 }

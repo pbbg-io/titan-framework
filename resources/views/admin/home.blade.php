7bcfe2a1fc5a2d6a35f0713aa894bc83e03056fa
Ian <hi@ian.pe>
2020-01-22 02:01:16 +0000

    Update make extension

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -50,31 +50,31 @@
                 </div>
             </div>
         </div>
-        <div class="col-sm-12 col-md-3">
-            <div class="card border-left-primary shadow h-100 py-2">
-                <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Version Check</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">
-                                @if($stats['version_update'])
-                                    {{ $stats['current_version'] }} - Latest: {{ $stats['latest_version'] }}
-                                @else
-                                    {{ $stats['current_version'] }}
-                                @endif
-                            </div>
-                        </div>
-                        <div class="col-auto">
-                            @if($stats['version_update'])
-                                <i class="fas fa-exclamation-triangle fa-2x text-danger"></i>
-                            @else
-                                <i class="fas fa-check fa-2x text-success"></i>
-                            @endif
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+{{--        <div class="col-sm-12 col-md-3">--}}
+{{--            <div class="card border-left-primary shadow h-100 py-2">--}}
+{{--                <div class="card-body">--}}
+{{--                    <div class="row no-gutters align-items-center">--}}
+{{--                        <div class="col mr-2">--}}
+{{--                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Version Check</div>--}}
+{{--                            <div class="h5 mb-0 font-weight-bold text-gray-800">--}}
+{{--                                @if($stats['version_update'])--}}
+{{--                                    {{ $stats['current_version'] }} - Latest: {{ $stats['latest_version'] }}--}}
+{{--                                @else--}}
+{{--                                    {{ $stats['current_version'] }}--}}
+{{--                                @endif--}}
+{{--                            </div>--}}
+{{--                        </div>--}}
+{{--                        <div class="col-auto">--}}
+{{--                            @if($stats['version_update'])--}}
+{{--                                <i class="fas fa-exclamation-triangle fa-2x text-danger"></i>--}}
+{{--                            @else--}}
+{{--                                <i class="fas fa-check fa-2x text-success"></i>--}}
+{{--                            @endif--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </div>--}}
     </div>
 
 @endsection
